63367ad335fd095be4d8b92276f6bbb9282b85c1
maksr51314 <maksr51314@gmail.com>
2013-04-06 19:23:58 +0300

    add css & status

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
 ko.observable.fn.beginEdit = function (transaction) {
-
     var self = this;
     var commitSubscription,
         rollbackSubscription;
-
-    // get the current value and store it for editing
     if (self.slice)
         self.editValue = ko.observableArray(self.slice());
     else
         self.editValue = ko.observable(self());
 
     self.dispose = function () {
-        // kill this subscriptions
         commitSubscription.dispose();
         rollbackSubscription.dispose();
     };
 
     self.commit = function () {
-        // update the actual value with the edit value
         self(self.editValue());
-
-        // dispose the subscriptions
         self.dispose();
     };
-
     self.rollback = function () {
-        // rollback the edit value
         self.editValue(self());
-
-        // dispose the subscriptions
         self.dispose();
     };
-
-    //  subscribe to the transation commit and reject calls
     commitSubscription = transaction.subscribe(self.commit,
         self,
         "commit");
@@ -43,51 +30,42 @@ ko.observable.fn.beginEdit = function (transaction) {
 
     return self;
 }
-
-
-function Item( name, desc, colour) {
+function Item( name, desc, behaviour, colour) {
     var self = this;
     self.name = ko.observable(name);
-    self.colour = ko.observable(colour);
     self.desc = ko.observable(desc);
+    self.behaviour = ko.observable(behaviour);
+    self.colour = ko.observable(colour);
 };
 
 Item.prototype.beginEdit = function(transaction) {
     this.name.beginEdit(transaction);
-    this.colour.beginEdit(transaction);
     this.desc.beginEdit(transaction);
+    this.colour.beginEdit(transaction);
 }
 
-
-/*----------------------------------------------------------------------*/
-/* View Model
- /*----------------------------------------------------------------------*/
-function FruitColourViewModel() {
+function ItemColourViewModel() {
     var self = this;
 
-    //  data
     self.availableColours = [];
     self.items = ko.observableArray([]);
     self.editingItem = ko.observable();
 
-    //  create the transaction for commit and reject
     self.editTransaction = new ko.subscribable();
 
-    //  helpers
     self.isItemEditing = function(item) {
         return item == self.editingItem();
     };
 
-    //  behaviour
-    self.addFruit = function () {
-        var item = new Item("New item","write your desc", self.availableColours[0]);
+    self.addItem = function () {
+        var item = new Item("New item","write your desc","Status",self.availableColours[0]);
         self.items.push(item);
 
         //  begin editing the new item straight away
-        self.editFruit(item);
+        self.editItem(item);
     };
 
-    self.removeFruit = function (item) {
+    self.removeItem = function (item) {
         if (self.editingItem() == null) {
             var answer = true; // confirm('Are you sure you want to delete this item? ' + item.name());
             if (answer) {
@@ -96,29 +74,20 @@ function FruitColourViewModel() {
         }
     };
 
-    self.editFruit = function (item) {
+    self.editItem = function (item) {
         if (self.editingItem() == null) {
-            // start the transaction
             item.beginEdit(self.editTransaction);
-
-            // shows the edit fields
             self.editingItem(item);
         }
     };
 
-    self.applyFruit = function (item) {
-        //  commit the edit transaction
+    self.applyItem = function () {
         self.editTransaction.notifySubscribers(null, "commit");
-
-        //  hides the edit fields
         self.editingItem(null);
     };
 
-    self.cancelEdit = function (item) {
-        //  reject the edit transaction
+    self.cancelEdit = function () {
         self.editTransaction.notifySubscribers(null, "rollback");
-
-        //  hides the edit fields
         self.editingItem(null);
     };
 
@@ -126,14 +95,14 @@ function FruitColourViewModel() {
 
 $(document).ready(function() {
     
-    var model = new FruitColourViewModel();
+    var model = new ItemColourViewModel();
     model.availableColours = ["Blue", "Green", "Orange", "Red", "Yellow"];
 
     var initData = [
-        new Item( "Apple","asdasd", "Green"),
-        new Item( "Banana","asdasd",  "Yellow"),
-        new Item( "Orange", "asdasd", "Orange"),
-        new Item( "Strawberry", "asdasd", "Red")
+        new Item( "Apple","asdasd","лллллл", "Green"),
+        new Item( "Banana","asdasd","лллллл",  "Yellow"),
+        new Item( "Orange", "asdasd","лллллл", "Orange"),
+        new Item( "Strawberry", "asdasd","лллллл", "Red")
     ];
 
     model.items(initData);
